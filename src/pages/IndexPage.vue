8f95577d0af7f589cba938d5e3695a27c006b0ff
mju6704101407 <Matinachowpa@gmail.com>
2025-10-08 09:50:05 +0700

    Edited IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,19 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="flex flex-center">
+    <q-form @submit.prevent="onSubmit" class="q-gutter-md">
+      <q-input filled label="Name" v-model="form.name" />
+      <q-input filled label="Email" v-model="form.email" type="email" />
+      <q-btn type="submit" label="Submit" color="primary" />
+    </q-form>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { ref } from 'vue'
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
+const form = ref({ name: '', email: '' })
 
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+function onSubmit() {
+  console.log('Form submitted', form.value)
+}
 </script>
